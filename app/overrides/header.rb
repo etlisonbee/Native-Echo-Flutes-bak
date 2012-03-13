93059fb790895cc97ca1890b87ee80927c2133cf
Errol Lisonbee <etlisonbee@gmail.com>
2012-03-13 17:54:32 -0600

    Initial commit

---
+++ app/overrides/header.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "newheader",
+                     :replace => "#header",
+                     :partial => "store/header")
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NativeEchoFlutes::Application.config.secret_token = 'b5f9b2c08980088985718599cf01f80491a748612f7309bd10c53efdf574f4cff0c058d8b2300d19594b20953d1028e8230cd19971e4020008df375c394dda7c'
