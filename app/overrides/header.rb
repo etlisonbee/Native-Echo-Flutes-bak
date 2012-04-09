9f361457d533f9b946a35b874f205d4fa13bdd3f
Errol Lisonbee <etlisonbee@gmail.com>
2012-04-08 18:48:13 -0600

    First changes for Native Echo Flutes

--- app/overrides/header.rb
+++ app/overrides/header.rb
@@ -2,3 +2,5 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "newheader",
                      :replace => "#header",
                      :partial => "store/header")
+
+
